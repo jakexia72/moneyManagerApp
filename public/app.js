3aa075e78693483e842295c501e083d05a9b67ac
jakexia72 <jakexia72@hotmail.com>
2019-08-03 23:15:02 -0400

    changed awkward js embed

--- public/app.js
+++ public/app.js
@@ -1,5 +1,18 @@
 
+// Your web app's Firebase configuration
+var config = {
+  apiKey: "AIzaSyBh4kYU7y5SutxOps2zSx4N679JPriUMQg",
+  authDomain: "moneytracker-b296e.firebaseapp.com",
+  databaseURL: "https://moneytracker-b296e.firebaseio.com",
+  projectId: "moneytracker-b296e",
+  storageBucket: "moneytracker-b296e.appspot.com",
+  messagingSenderId: "287763511011",
+  appId: "1:287763511011:web:0add0c0702a8afbf"
+};
+// Initialize Firebase
+firebase.initializeApp(config);
 
+const db = firebase.firestore();
 // var username = db.ref('users/' + userId + '/name');
 //
 // // var userId = "PDb2N4jbQKyk0GDemeBL";
