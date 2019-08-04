1f95af23dcb9328ec9b759676998e9448a5d63d9
jakexia72 <jakexia72@hotmail.com>
2019-08-03 22:59:17 -0400

    firebase is poppin'

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const app = express();
 const port = process.env.PORT ||  7890;
 const mysql = require('mysql');
 const cors = require('cors');
-
+const firebase = require('firebase/database');
 const router = require('./routes/expense.js');
 app.use(router);
 
+app.use(express.static(__dirname + '/public'));
+
 const path = __dirname;
 
+app.get('/', (req, res) =>{
+  console.log("running firebase front end");
+  res.sendFile(__dirname + "/index.html")
+});
 
 app.listen(port, () => {
   console.log("server is up and listening on " + port);
---
+++ public/app.js
@@ -0,0 +1,50 @@
+
+
+// var username = db.ref('users/' + userId + '/name');
+//
+// // var userId = "PDb2N4jbQKyk0GDemeBL";
+// // var userPath = "/users"/ + userId + "/";
+//
+// // db.ref('/users/' + userId).once('value').then(function(snapshot) {
+// //   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+// //   $("greeting").html = username;
+// // });
+//
+// $("greeting").html = username;
+
+
+
+//var userExpenses = firebase.database().ref(userPath + "expenses" + )
+var userId = "PDb2N4jbQKyk0GDemeBL";
+
+var today = new Date();
+var time = today.getHours();
+
+function timeOfDay(){
+  if(time < 12){
+    return "Morning";
+  } else if (time < 17){
+    return "Afternoon";
+  } else if (time < 24){
+    return "Evening";
+  }
+}
+// var userPath = "/users"/ + userId + "/";
+
+// db.ref('/users/' + userId).once('value').then(function(snapshot) {
+//   var username = (snapshot.val() && snapshot.val().username) || 'Anonymous';
+//   $("greeting").html = username;
+// });
+// var username = db.ref('users/' + userId + '/name');
+db.collection("users").doc(userId).get().then((snapshot) => {
+  const uname = snapshot.data().name
+  $("#greeting").html("Good " + timeOfDay() + " " + uname.substr(0, uname.indexOf(' ')));
+});
+
+
+
+db.collection("users").doc(userId).collection("expenses").get().then((snapshot) => {
+    snapshot.docs.forEach((doc) => {
+      console.log(doc.data());
+    })
+});
