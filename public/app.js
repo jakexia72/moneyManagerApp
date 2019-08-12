a847f894c0eaaa050ea25d21f458a4a9090420dc
JakeFromWork <jakexia72@hotmail.com>
2019-08-12 16:48:25 -0400

    minor changes

--- public/app.js
+++ public/app.js
@@ -24,33 +24,7 @@ db.collection("users").doc(userId).collection("expenseTypes").get().then((snapsh
 
 
 
-//Add a new expense to db
-$('#add-expense-form').submit(function(e){
-  e.preventDefault();
-  var expenseDateVar = new Date();
-    // console.log("expnse is: " + $('#add-expenseAmount'));
-    // console.log("expnse prsed is: " + parseFloat($('#add-expenseAmount').val()));
-  db.collection("users").doc(userId).collection("expenses").add({
-    expenseAmount: parseFloat($('#add-expenseAmount').val()),
-    expenseName: $('#add-expenseName').val(),
-    expenseLocation: $('#add-expenseLocation').val(),
-    expenseType: $('#add-expenseType').val(),
-    expenseDate: expenseDateVar
-  });
-   $('#add-expense-form').trigger('reset');
-});
 
-//Add a new user-defined expenseType
-$('#add-expenseType-form').submit(function(e){
-  e.preventDefault();
-  db.collection("users").doc(userId).collection("expenseTypes").add({
-    typeName: $('#add-expenseTypeName').val(),
-    typeColor: $('#add-expenseTypeColor').val(),
-    budgetPeriod: $('#add-expenseTypeBudgetPeriod').val(),
-    budget: $('#add-expenseTypeBudget').val()
-  });
-  $('#add-expenseType-form').trigger('reset');
-});
 
 //Creating the greeting
 db.collection("users").doc(userId).get().then((snapshot) => {
--- public/uiInteractions.js
+++ public/uiInteractions.js
@@ -12,6 +12,35 @@ function checkHasBudget(value){
 
 $(document).ready(function() {
 
+  //Add a new expense to db
+  $('#add-expense-form').submit(function(e){
+    e.preventDefault();
+    var expenseDateVar = new Date();
+      // console.log("expnse is: " + $('#add-expenseAmount'));
+      // console.log("expnse prsed is: " + parseFloat($('#add-expenseAmount').val()));
+    db.collection("users").doc(userId).collection("expenses").add({
+      expenseAmount: parseFloat($('#add-expenseAmount').val()),
+      expenseName: $('#add-expenseName').val(),
+      expenseLocation: $('#add-expenseLocation').val(),
+      expenseType: $('#add-expenseType').val(),
+      expenseDate: expenseDateVar
+    });
+     $('#add-expense-form').trigger('reset');
+  });
+
+  //Add a new user-defined expenseType
+  $('#add-expenseType-form').submit(function(e){
+    console.log("RAN");
+    e.preventDefault();
+    db.collection("users").doc(userId).collection("expenseTypes").add({
+      typeName: $('#add-expenseTypeName').val(),
+      typeColor: $('#add-expenseTypeColor').val(),
+      budgetPeriod: $('#add-expenseTypeBudgetPeriod').val(),
+      budget: $('#add-expenseTypeBudget').val()
+    });
+    $('#add-expenseType-form').trigger('reset');
+  });
+
   $('#categories-add').click(function(){
     console.log("CLICKED");
     if($('#categories-add').data("id") == "categories-add-closed"){
